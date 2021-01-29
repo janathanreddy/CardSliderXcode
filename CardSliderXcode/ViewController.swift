13c1eea0ad1a74fa60d04aac92d3824646fd8f09
janarthan s <janathanreddy@gmail.com>
2021-01-29 18:30:10 +0530

    Initial Commit

---
+++ CardSliderXcode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CardSliderXcode
+//
+//  Created by Janarthan Subburaj on 29/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
